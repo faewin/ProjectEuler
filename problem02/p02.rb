3c2779c250d4c46c965fbdfef1bcc11ae1ca74e1
Edward <feek1g@gmail.com>
2015-04-22 17:14:17 -0700

    added solution in ruby

---
+++ problem02/p02.rb
@@ -0,0 +1,11 @@
+def solution
+  fab = [1,2]
+  result = []
+  until fab.last > 4_000_000
+    next_term = fab[-1] + fab[-2]
+    result = fab.push(next_term)
+  end
+  result.select{|num| num % 2 == 0}.reduce(:+)
+end
+
+p solution
