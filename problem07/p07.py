733f0b6cd54798f802ca84d5d87ef4365b856556
Edward <feek1g@gmail.com>
2015-04-27 21:46:26 -0700

    adds py sol

--- problem07/p07.py
+++ problem07/p07.py
@@ -2,9 +2,19 @@ import math
 
 def isPrime(num):
     sqrt = int(math.ceil(math.sqrt(num)))
-    for i in range(2,sqrt+1):
+    for i in range(2,sqrt + 1):
         if num % i == 0:
             return False
     return True
 
+def solution():
+    primes = []
+    lpn = 2
 
+    while 10000 > len(primes):
+        if isPrime(lpn):
+            primes.append(lpn)
+        lpn += 1
+    return primes[-1]
+
+print solution()
