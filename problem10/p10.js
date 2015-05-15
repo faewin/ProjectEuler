f85f6acb8b5226a8ae50da88a0e9868618179bdf
Edward <feek1g@gmail.com>
2015-05-15 13:41:47 -0700

    adds isPrime function

---
+++ problem10/p10.js
@@ -0,0 +1,13 @@
+function isPrime(num) {
+  if (num <= 1) {
+    return false;
+  }
+  var sqNum = Math.sqrt(num);
+  for (var i = 2; i < sqNum; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
